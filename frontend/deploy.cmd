ce6c960b9f647a004a7d79708054f9f144149b59
chenwei <294098494@qq.com>
2021-11-29 18:58:38 +0800

    deploy

--- frontend/deploy.cmd
+++ frontend/deploy.cmd
@@ -1,8 +1,8 @@
 start /WAIT /b git push --delete origin gh-pages
-pushd ./dist
+Pushd dist
 start /WAIT /b git init
 start /WAIT /b git remote add origin git@github.com:chansssss/id-photo.git
 start /WAIT /b git add . -A
 start /WAIT /b git commit -m'deploy'
 start /WAIT /b git checkout -b gh-pages
-start /WAIT /b git push
+start /WAIT /b git push --set-upstream origin gh-pages
